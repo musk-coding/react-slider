c6e0bc42449f3f8162204dd23b2af46cfdaf3058
Othmane <othmane.abouelyzza@gmail.com>
2021-02-05 23:41:29 +0100

    Slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Slider from "./components/Slider";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    images: [
+      "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+      "https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80",
+      "https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80",
+      "https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80",
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <Slider slides={this.state.images} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Slider.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import SliderContent from "./SliderContent";
+import "./Slider.css";
+import Slide from "./Slide";
+import Arrow from "./Arrow";
+
+export default class Slider extends Component {
+  state = {
+    activeIndex: 0,
+    translate: 0,
+    transition: 0.45,
+  };
+
+  nextSlide = () => {
+    if (this.state.activeIndex === this.props.slides.length - 1) {
+      return this.setState({
+        ...this.state,
+        translate: 0,
+        activeIndex: 0,
+        transition: 0,
+      });
+    }
+
+    this.setState({
+      ...this.state,
+      activeIndex: this.state.activeIndex + 1,
+      translate: (this.state.activeIndex + 1) * this.getWidth(),
+      transition: 0.45,
+    });
+  };
+
+  prevSlide = () => {
+    if (this.state.activeIndex === 0) {
+      return this.setState({
+        ...this.state,
+        translate: (this.props.slides.length - 1) * this.getWidth(),
+        activeIndex: this.props.slides.length - 1,
+        transition: 0,
+      });
+    }
+
+    this.setState({
+      ...this.state,
+      activeIndex: this.state.activeIndex - 1,
+      translate: (this.state.activeIndex - 1) * this.getWidth(),
+      transition: 0.45,
+    });
+  };
+
+  getWidth = () => {
+    return window.innerWidth;
+  };
+
+  render() {
+    const { translate, transition } = this.state;
+
+    return (
+      <div className="slider">
+        <SliderContent
+          translate={translate}
+          transition={transition}
+          width={this.getWidth() * this.props.slides.length}
+        >
+          {this.props.slides.map((slide, i) => (
+            <Slide key={slide + i} content={slide} />
+          ))}
+        </SliderContent>
+        <Arrow direction="left" handleClick={this.prevSlide} />
+        <Arrow direction="right" handleClick={this.nextSlide} />
+      </div>
+    );
+  }
+}
